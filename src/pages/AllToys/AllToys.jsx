173808735927488c803fb668d6d7211c035da8f9
SimantaSarker <simantasarker0000@gmail.com>
2023-05-18 20:38:14 +0600

    Search system done

--- src/pages/AllToys/AllToys.jsx
+++ src/pages/AllToys/AllToys.jsx
@@ -1,11 +1,31 @@
-import { useLoaderData } from "react-router-dom";
+
 import AllToysRow from "../AddToys/AllToysRow";
+import { useEffect, useState } from "react";
 
 const AllToys = () => {
-  const allToys=useLoaderData();
+  const [allToys,setAllToys]=useState([]);
+  const [search,setSearch]=useState("");
+
+useEffect(()=>{
+  fetch("http://localhost:5000/toys")
+  .then((res)=>res.json())
+  .then((data)=>setAllToys(data))
+},[])
+
+
+const handleToyNameSearch=()=>{
+  fetch(`http://localhost:5000/toysSearchByToyName/${search}`)
+  .then((res)=>res.json()).then((data)=>setAllToys(data))
+  
+}
+
 
   return (
   <div className="overflow-x-auto my-2 ">
+    <div>
+      <input type="text"  className="text-center p-1 bg-red-400" onChange={(e)=>setSearch(e.target.value)} />
+      <button onClick={handleToyNameSearch}>Search</button>
+    </div>
   <table className="table w-full">
     {/* head */}
     <thead>
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -28,7 +28,7 @@ const router = createBrowserRouter([
       {
         path:"allToys",
         element:<AllToys></AllToys>,
-        loader:()=>fetch("http://localhost:5000/toys")
+        
       },
       {
         path:"myToys",
