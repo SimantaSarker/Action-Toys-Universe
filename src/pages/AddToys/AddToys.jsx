af0f78da38eb4934cdf9ccfa2cf2773a08499c19
SimantaSarker <simantasarker0000@gmail.com>
2023-05-18 16:59:35 +0600

    Sweet alert showing after data added mongodb

--- src/pages/AddToys/AddToys.jsx
+++ src/pages/AddToys/AddToys.jsx
@@ -1,38 +1,66 @@
 import { useContext } from "react";
 import { AuthContext } from "../../providers/AuthProvider";
+import Swal from "sweetalert2";
 
 const AddToys = () => {
   const { user } = useContext(AuthContext);
 
-const handleAddToys=(event)=>{
-  event.preventDefault();
-  const form=event.target;
-  const name=form.name.value;
-  const quantity=form.quantity.value;
-  const seller=form.seller.value;
-  const email=form.email.value;
-  const price=form.price.value;
-  const rating=form.rating.value;
-  const subCategory=form.categories.value;
-  const photo=form.photo.value;
-  console.log(name,email,quantity,seller,price,rating,subCategory,photo);
-  const toy={
-    name,email,quantity,seller,price,rating,subCategory,photo
-  }
-  fetch('http://localhost:5000/toys',{
-    method:"POST",
-    headers:{
-      "content-type":"application/json"
-    },
-    body:JSON.stringify(toy)
-  })
-  .then((res)=>res.json())
-  .then((data)=>console.log(data))
-}
-  
+  const handleAddToys = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const quantity = form.quantity.value;
+    const seller = form.seller.value;
+    const email = form.email.value;
+    const price = form.price.value;
+    const rating = form.rating.value;
+    const subCategory = form.categories.value;
+    const photo = form.photo.value;
+    console.log(
+      name,
+      email,
+      quantity,
+      seller,
+      price,
+      rating,
+      subCategory,
+      photo
+    );
+    const toy = {
+      name,
+      email,
+      quantity,
+      seller,
+      price,
+      rating,
+      subCategory,
+      photo,
+    };
+    fetch("http://localhost:5000/toys", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(toy),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if(data.insertedId)
+        {
+          Swal.fire({
+            position: 'center',
+            icon: 'success',
+            title: 'Successfully insert data into database',
+            showConfirmButton: false,
+            timer: 1500
+          })
+          form.reset();
+        }
+      });
+  };
+
   return (
     <form onSubmit={handleAddToys}>
-    
       <div className="md:flex md:gap-3 mt-10">
         <div className="form-control md:w-1/2">
           <label className="label">
@@ -89,7 +117,7 @@ const handleAddToys=(event)=>{
             <input
               type="email"
               name="email"
-            readOnly
+              readOnly
               defaultValue={user?.email}
               placeholder="Enter Seller Email "
               className="input input-bordered w-full"
@@ -97,11 +125,6 @@ const handleAddToys=(event)=>{
           </label>
         </div>
       </div>
-
-
-
-
-
 
       <div className="md:flex md:gap-3 mt-10">
         <div className="form-control md:w-1/2">
@@ -132,12 +155,6 @@ const handleAddToys=(event)=>{
         </div>
       </div>
 
-
-
-
-
-
-
       {/* form row with category and details */}
       <div className="md:flex md:gap-3">
         <div className="form-control w-1/2 mx-auto my-3 text-xl">
@@ -159,7 +176,7 @@ const handleAddToys=(event)=>{
             <input
               type="text"
               name="details"
-              placeholder="Enter Coffee Details "
+              placeholder="Enter  Details "
               className="input input-bordered w-full"
             />
           </label>
@@ -182,7 +199,6 @@ const handleAddToys=(event)=>{
         </div>
       </div>
 
-      {/* submit butto */}
       <div>
         <input
           type="submit"
